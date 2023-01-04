8b873e7a9543c876e76882bc21e4ff6b52788709
Rodrigo Santos <105660050+jrodrigosantosm@users.noreply.github.com>
2023-01-04 16:59:14 -0300

    new

--- Ex cadastro de devs/index.js
+++ Ex cadastro de devs/index.js
@@ -1,54 +1,68 @@
-const btnNovaTec = document.getElementById("btnNovatec")
+let form = document.getElementById('formNovaTec')
+let ul = document.getElementById('ul')
 
-const lista = []
+let btnNewtec = document.getElementById('btnNewtec')
+//formulario e nome do input
+let newTec = []
+//lista de tecnologias
 
-const name = document.getElementById('name').value
-
-    btnNovaTec.addEventListener("submit", function (ev){
+btnNewtec.addEventListener('click', function(ev){
     ev.preventDefault()
+    let li = document.createElement('li')
+    let inputNewtec = document.createElement('input')
+        inputNewtec.type = 'text'
+        inputNewtec.className = 'inputNewtec'
+    let labelNewtec = document.createElement('label')
+        labelNewtec.innerText = 'Nova tecnologia: '
+    //campo de nome de nova tecnologia
 
-        const div = document.getElementById("div")
-
-        const form = document.createElement('li')
-
-        const label = document.createElement('label')
-        label.innerText = 'Nova tecnología '
-        const input = document.createElement("input")
-
-        const linha = document.createElement('br')
-
-        const labelRadio = document.createElement('label')
-        labelRadio.innerText = '\nExperiência: '
-
-        const inputRadio1 = document.createElement('input')
-        inputRadio1.type = 'radio'
-        inputRadio1.name = 'exp'
-        inputRadio1.value = '2 anos'
-        const labelRadio1 = document.createElement('label')
-        labelRadio1.innerText = ' 2 anos     /'
-
-        const inputRadio2 = document.createElement('input')
-        inputRadio2.type = 'radio'
-        inputRadio2.name = 'exp'
-        inputRadio2.value = '5 anos'
-        const labelRadio2 = document.createElement('label')
-        labelRadio2.innerText = ' 5 anos\n'
-
-        const btnRemove = document.createElement('button')
-        btnRemove.type = 'button'
-        btnRemove.innerText = 'Remover Tecnologia'
-        btnRemove.addEventListener("click", function(){
-                document.removeChild(form)
-            })
-
+    let labelexp = document.createElement('label')
+        labelexp.innerText = 'Exeperiência:  '
+    //campos de experiência:
+        let inputradio1 = document.createElement('input')
+                inputradio1.type ='radio'
+                inputradio1.value = 'Até 2 anos'
+                inputradio1.className = 'inputradio'
+            let labelradio1 = document.createElement('label')
+                labelradio1.innerText = 'Até 2 anos: '
+        //opção 1 
+        let inputradio2 = document.createElement('input')
+                inputradio2.type ='radio'
+            let labelradio2 = document.createElement('label')
+                labelradio2.innerText = '   3 - 5 anos  '
+                inputradio2.value = '3 - 5 anos'
+                inputradio2.className = 'inputradio'
+        //opção 2
+        let inputradio3 = document.createElement('input')
+                inputradio3.type ='radio'
+            let labelradio3 = document.createElement('label')
+                labelradio3.innerText = '   Mais de 5 anos  '
+                inputradio3.value = 'Mais de 5 anos'
+                inputradio3.className = 'inputradio'
+        //opção 3   
         
+    let del = document.createElement('button')
+        del.type ='button'
+        del.innerText = 'Deletar tecnologia'
+        del.addEventListener('click', function(){
+            ul.removeChild(li)
+        })
 
-
-        form.append(label, input, labelRadio, inputRadio1, labelRadio1, inputRadio2, labelRadio2, btnRemove)
-
-        div.append(form)
+    li.append(labelNewtec, inputNewtec, labelexp, labelradio1 ,inputradio1, labelradio2, inputradio2, labelradio3, inputradio3, del)
+    ul.append(li)
 })
 
-
-
-
+let salvar = document.getElementById('btnsalvar')
+salvar.addEventListener('click', function(ev){
+    ev.preventDefault()
+    let name = document.getElementById('name').value
+    let lista = []
+    newtecn = document.getElementsByClassName('inputNewtec').value
+    let dev ={
+        Nome : name,
+        tecnologia : newTec
+        
+    }
+   lista.push(dev)
+console.log({lista})    
+})
